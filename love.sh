547f4a81b7ea903f68536c2138c29a0aae1e2488
askinss <faskinss@gmail.com>
2015-02-11 22:38:56 +0100

    added figlet command

--- love.sh
+++ love.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #The iteration generate some love with different colors on terminal
-
+figlet I HAVE LOTS OF 
  for (( i = 1; i < 10; i++ )); do echo  -e "$(tput setaf $i)     $..    ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)   $..  0  0   $10 $(tput sgr0)"; echo  -e "$(tput setaf $i)  $..    0     ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i) $..           ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i) $..          ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)  $..        ..$10 $(tput sgr0)";   echo  -e "$(tput setaf $i)  $..       ..$10 $(tput sgr0)";  echo  -e "$(tput setaf $i)   $..     ..$10 $(tput sgr0)";  echo  -e "$(tput setaf $i)     $..  .$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)        0 $(tput sgr0)";   done
-
-
+figlet for U
+figlet "HAPPY VALENTINE"
