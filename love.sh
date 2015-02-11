c1eb8de6337c316d266835ff5a8472abccf0a99c
askinss <faskinss@gmail.com>
2015-02-11 22:16:43 +0100

    readme modified

--- love.sh
+++ love.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+#The iteration generate some love with different colors on terminal
 
  for (( i = 1; i < 10; i++ )); do echo  -e "$(tput setaf $i)     $..    ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)   $..  0  0   $10 $(tput sgr0)"; echo  -e "$(tput setaf $i)  $..    0     ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i) $..           ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i) $..          ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)  $..        ..$10 $(tput sgr0)";   echo  -e "$(tput setaf $i)  $..       ..$10 $(tput sgr0)";  echo  -e "$(tput setaf $i)   $..     ..$10 $(tput sgr0)";  echo  -e "$(tput setaf $i)     $..  .$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)        0 $(tput sgr0)";   done
 
