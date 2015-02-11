78d6927f545da3e6f466c0611b819b7398821394
askinss <faskinss@gmail.com>
2015-02-11 22:05:39 +0100

    removed a bad character

--- love.sh
+++ love.sh
@@ -2,7 +2,6 @@
 
 
 
-for (( i = 0; i < 10; i++ )); do echo  -e "$(tput setaf $i)     $..    ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)   $..  0  0   $10 $(tput sgr0)"; echo  -e "$(tput setaf $i)  $..    0     ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i) $..           ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i) $..          ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)  $..        ..$10 $(tput sgr0)";   echo  -e "$(tput setaf $i)  $..       ..$10 $(tput sgr0)";  echo  -e "$(tput setaf $i)   $..     ..$10 $(tput sgr0)";  echo  -e "$(tput setaf $i)     $..  .$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)        0 $(tput sgr0)";   done
-
+ for (( i = 1; i < 10; i++ )); do echo  -e "$(tput setaf $i)     $..    ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)   $..  0  0   $10 $(tput sgr0)"; echo  -e "$(tput setaf $i)  $..    0     ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i) $..           ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i) $..          ..$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)  $..        ..$10 $(tput sgr0)";   echo  -e "$(tput setaf $i)  $..       ..$10 $(tput sgr0)";  echo  -e "$(tput setaf $i)   $..     ..$10 $(tput sgr0)";  echo  -e "$(tput setaf $i)     $..  .$10 $(tput sgr0)"; echo  -e "$(tput setaf $i)        0 $(tput sgr0)";   done
 
 
